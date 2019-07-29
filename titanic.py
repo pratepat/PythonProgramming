a8a8ca81ca312ac1e1d3ec5adb8044b48900cb85
Prateek <ppathak1611@gmail.com>
2019-07-29 19:50:33 +0530

    Initial

--- titanic.py
+++ titanic.py
@@ -4,3 +4,5 @@ import pandas as pd
 #titanic = pd.read_csv('https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv')
 titanic = pd.read_csv("titanic.csv")
 titanic.head()
+
+3//2
